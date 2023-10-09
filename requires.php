7045a76bdbae324f5cd8a9f28cbd976a9e4bae9e
Alex W3O <alex@w3o.com>
2023-10-09 14:13:18 +0300

    small updates

--- requires.php
+++ requires.php
@@ -11,9 +11,13 @@
                     throw new \Exception("Bad ROOT_DIR specified: " . ROOT_DIR);
                 }
             } else {
-                $rootDir = \getcwd();
-                if (\is_dir($rootDir . '/vendor')) {
-                    \define('ROOT_DIR', $rootDir);
+                $rootDir =\strtr(\getcwd(), '\\', '/') . '/vendor';
+                while($i = \strrpos($rootDir, '/vendor')) {
+                    $rootDir = \substr($rootDir, 0, $i);
+                    if (\is_dir($rootDir . '/vendor')) {
+                        \define('ROOT_DIR', $rootDir);
+                        break;
+                    }
                 }
             }
         }
@@ -24,22 +28,23 @@
             echo "NameSpaces Successful Updated from Composer\n";
         }
         
-        $mySrcDir = __DIR__ . '/src';
-        // Load own classes and traits
-        require_once $mySrcDir . '/ComposerWorks.php';
-        require_once $mySrcDir . '/DownLoader.php';
-        $fullClassFile = \strtr($mySrcDir, '\\', '/') . '/' . $classShortName . '.php';
-        require_once $fullClassFile;
+        // Code for diagnostic, not required
+        // $mySrcDir = __DIR__ . '/src';
+        // // Load own classes and traits
+        // require_once $mySrcDir . '/ComposerWorks.php';
+        // require_once $mySrcDir . '/DownLoader.php';
+        // $fullClassFile = \strtr($mySrcDir, '\\', '/') . '/' . $classShortName . '.php';
+        // require_once $fullClassFile;
         
-        // AutoLoader diagnostic
-        $ourAutoLoadClass = $reqManClass::OUR_AUTO_LOAD_CLASS;
-        if (!\class_exists($ourAutoLoadClass, false)) {
-            throw new \Exception("Autoloader diagnostic error: NO autoload class $ourAutoLoadClass");
-        }
-        $chkFile = $ourAutoLoadClass::autoLoad($reqManClass, null);
-        if ($chkFile && (!\is_string($chkFile) || \strtr($chkFile, '\\', '/') !== $fullClassFile)) {
-            throw new \Exception("Autoloader diagnostic error:\n $chkFile \n $fullClassFile \n");
-        }
+        // // AutoLoader diagnostic
+        // $ourAutoLoadClass = $reqManClass::OUR_AUTO_LOAD_CLASS;
+        // if (!\class_exists($ourAutoLoadClass, false)) {
+        //     throw new \Exception("Autoloader diagnostic error: NO autoload class $ourAutoLoadClass");
+        // }
+        // $chkFile = $ourAutoLoadClass::autoLoad($reqManClass, null);
+        // if ($chkFile && (!\is_string($chkFile) || \strtr($chkFile, '\\', '/') !== $fullClassFile)) {
+        //     throw new \Exception("Autoloader diagnostic error:\n $chkFile \n $fullClassFile \n");
+        // }
 
         if (!\class_exists($reqManClass, true)) {
             throw new \Exception("Class $reqManClass was not loaded, required");
@@ -47,5 +52,6 @@
     }
 
     $obj = new $reqManClass();
+    $obj->echoOn = true;
     $obj->run();
 })('RequireManager');
