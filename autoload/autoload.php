f0736c0ba4e1d8bf7ec30464f77615dcae91b464
Alex W3O <alex@w3o.com>
2023-10-09 08:33:00 +0300

    adaptate by composer calling

--- autoload/autoload.php
+++ autoload/autoload.php
@@ -1,10 +1,17 @@
 <?php
-$myPkgDir   = \dirname(__FILE__, 2);
-$rootDir    = \defined('ROOT_DIR')   ? ROOT_DIR    : \dirname($myPkgDir, 2);
-$rootDir    = \trim(strtr($rootDir, '\\', '/'), '/');
-$vendorDir  = \defined('VENDOR_DIR') ? VENDOR_DIR  : $rootDir . '/vendor';
-$classesDir = \defined('CLASSES_DIR')? CLASSES_DIR : $rootDir . '/includes/classes';
-$extDir     = \defined('EXT_FS_DIR') ? EXT_FS_DIR  : $rootDir . '/ext';
-require_once __DIR__ . "/AutoLoadSetup.php";
+(function($file) {
+    if ($file) {
+        $rootDir = \strtr($file, '\\', '/');
+        $i = \strpos($rootDir, '/vendor/composer');
+        $rootDir = \defined('ROOT_DIR') ? ROOT_DIR : \substr($rootDir, 0, $i ? $i : 0);
+    } else {
+        $rootDir = \defined('ROOT_DIR') ? ROOT_DIR : \dirname(__FILE__, 5);
+    }
+    $rootDir    = \trim(strtr($rootDir, '\\', '/'), '/');
+    $vendorDir  = \defined('VENDOR_DIR') ? VENDOR_DIR  : $rootDir . '/vendor';
+    $classesDir = \defined('CLASSES_DIR')? CLASSES_DIR : $rootDir . '/includes/classes';
+    $extDir     = \defined('EXT_FS_DIR') ? EXT_FS_DIR  : $rootDir . '/ext';
+    require_once __DIR__ . "/AutoLoadSetup.php";
 
-(new \dynoser\autoload\AutoLoadSetup($rootDir, $vendorDir, $classesDir, $extDir));
+    (new \dynoser\autoload\AutoLoadSetup($rootDir, $vendorDir, $classesDir, $extDir));
+})($file ?? '');// $file is Composer value
